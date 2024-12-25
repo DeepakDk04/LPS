e9ce2457806fd61b99f95e3f080446d3d98cf9a7
Deepak <deepak99pmk@gmail.com>
2024-12-25 15:43:52 +0530

    added vendor model

--- loypt/loypt/settings.py
+++ loypt/loypt/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    # custom modules
+    # custom apps
+    "vendor",
 ]
 
 MIDDLEWARE = [
---
+++ loypt/vendor/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Vendor(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=20)
+    about = models.CharField(max_length=60)
+    address = models.CharField(max_length=120)
+    pincode = models.CharField(max_length=6)
+    state = models.CharField(max_length=20)
+    contactNo = models.CharField(max_length=13)
+    sharedKey = models.CharField(max_length=30)
+    apiKey = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f"{self.user.get_username()} from {self.name}"
