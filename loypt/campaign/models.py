9df6e4e1e5b2b9db897c62cc3a6f00035c78e814
Deepak <deepak99pmk@gmail.com>
2024-12-25 18:09:26 +0530

    added campaign app

---
+++ loypt/campaign/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from rule.models import Rule
+from vendor.models import Vendor
+
+
+class Campaign(models.Model):
+
+    campaignStatus = [
+        ("notready", "notready"),
+        ("active", "active"),
+        ("inactive", "inactive"),
+        ("complete", "complete"),
+        ("extend", "extend"),
+    ]
+    name = models.CharField(max_length=20)
+    description = models.CharField(max_length=50)
+    activeRules = models.ManyToManyField(Rule, related_name="rulelist")
+    rulesHistory = models.ManyToManyField(Rule, related_name="pastrule")
+    createdBy = models.ForeignKey(Vendor, on_delete=models.CASCADE)
+    createdOn = models.DateTimeField(auto_now_add=True)
+    startDate = models.DateField()
+    endDate = models.DateField()
+    status = models.CharField(max_length=10, choices=campaignStatus, default="notready")
+    campaignType = models.CharField(max_length=20, default="user-campaign")
+    isCumilative = models.BooleanField(default=False)
+    targetPoints = models.PositiveIntegerField(default=1)
+    targetPurchases = models.PositiveIntegerField(default=1)
+    pointsGenerated = models.PositiveIntegerField(default=0)
+    purchasesCount = models.PositiveIntegerField(default=0)
+    code = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
--- loypt/loypt/settings.py
+++ loypt/loypt/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "vendor",
     "customer",
     "rule",
+    "campaign",
 ]
 
 MIDDLEWARE = [
