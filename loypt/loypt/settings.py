665c4949d16b47f2d9cc1a54487fb1721d688da3
Deepak <deepak99pmk@gmail.com>
2024-12-25 16:59:51 +0530

    added rule app

--- loypt/loypt/settings.py
+++ loypt/loypt/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     # custom apps
     "vendor",
     "customer",
+    "rule",
 ]
 
 MIDDLEWARE = [
---
+++ loypt/rule/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from vendor.models import Vendor
+
+
+class Rule(models.Model):
+
+    name = models.CharField(max_length=20)
+    Description = models.CharField(max_length=40)
+    isDerivePurchaseVal = models.BooleanField(default=False)
+    purchasePercentage = models.PositiveSmallIntegerField(default=0)
+    isPointLimit = models.BooleanField(default=False)
+    pointLimit = models.PositiveSmallIntegerField(default=0)
+    presetPoint = models.PositiveSmallIntegerField(default=0)
+    daysToExpire = models.PositiveSmallIntegerField(default=100)
+    isActive = models.BooleanField(default=True)
+    ruleType = models.CharField(max_length=20, default="user-rule")
+    isReuseable = models.BooleanField(default=True)
+    createdBy = models.ForeignKey(Vendor, on_delete=models.CASCADE)
+    createdOn = models.DateTimeField(auto_now_add=True)
+    lastUpdatedOn = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
