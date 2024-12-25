77a9d698659d4d551a8947392b4fcf0cde8db88c
Deepak <deepak99pmk@gmail.com>
2024-12-25 18:59:16 +0530

    added purchase transaction module

--- loypt/loypt/settings.py
+++ loypt/loypt/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     "customer",
     "rule",
     "campaign",
+    "purchase",
 ]
 
 MIDDLEWARE = [
---
+++ loypt/purchase/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from campaign.models import Campaign
+from customer.models import Customer
+from rule.models import Rule
+from vendor.models import Vendor
+
+
+class Purchase(models.Model):
+    """
+    Read-Only Model & Should not be updated
+    """
+
+    # todo: to fix on delete default when the reference object deleted
+    campaign = models.ForeignKey(Campaign, on_delete=models.CASCADE)
+    rule = models.ForeignKey(Rule, on_delete=models.CASCADE)
+    amount = models.FloatField()
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
+    createdOn = models.DateTimeField(auto_now_add=True)
+
+    def save(self, **kwargs):
+        # todo: condition to validate the update
+        super().save(**kwargs)
+
+    def __str__(self):
+        return f"{self.customer} - {self.amount}"
