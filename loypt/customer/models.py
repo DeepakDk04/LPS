1454dcb55165bfef9394669717ac88ab5d0a748a
Deepak <deepak99pmk@gmail.com>
2024-12-25 16:59:12 +0530

    added  last update on field

--- loypt/customer/models.py
+++ loypt/customer/models.py
@@ -5,8 +5,9 @@ from django.contrib.auth.models import User
 class Customer(models.Model):
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    mobile = models.CharField(max_length=13)
-    pointsRemaining = models.IntegerField(default=0)
+    mobile = models.CharField(max_length=13, unique=True)
+    pointsRemaining = models.SmallIntegerField(default=0)
+    lastUpdatedOn = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.user.get_username()
