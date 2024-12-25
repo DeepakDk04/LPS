880dea82eca76581c3eaed5b670b66f6e9a33824
Deepak <deepak99pmk@gmail.com>
2024-12-26 02:15:54 +0530

    added group support for customer obj

--- loypt/customer/models.py
+++ loypt/customer/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 
 
 class Customer(models.Model):
@@ -11,3 +11,10 @@ class Customer(models.Model):
 
     def __str__(self):
         return self.user.get_username()
+
+    def save(self, **kwargs):
+        customerGroup, created = Group.objects.get_or_create(name="Customer")
+        if created:
+            print("Customer Group Created")
+        self.user.groups.add(customerGroup)
+        return super().save(**kwargs)
